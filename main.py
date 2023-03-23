6c8b3509077ba76765fdba413d756138d91de367
anzarpe <anders011097@gmail.com>
2023-03-23 11:22:38 -0400

    Se mejoro la parte de los botones, se organizaron los iconos, se cambiaron algunos icono y se añadio un Frame para el Inicio

--- main.py
+++ main.py
@@ -6,47 +6,56 @@ root = tk.Tk()
 
 root.geometry("1200x666")
 root.title("Registro Civil")
-root.iconbitmap("Images/Pen.ico")
-root.config(background="#ADAAAB")
-root.resizable(0,0)
+root.iconbitmap("Images/ImagenesFunciones/Pen.ico")
+root.config(background="WHITE")
+root.resizable(1,1)
+
+# Frames
+
+frameMenu = tk.Frame(root)
+frameMenu.pack(side=tk.LEFT)
+
+frameInicio = tk.Frame(root,background="#209cb4",height = 666, width = 1062,highlightbackground="WHITE",highlightthickness=5)
+frameInicio.pack(side=tk.TOP)
 
 # Imagenes Botones
 
-nacimientoPhoto = tk.PhotoImage(file="Images/ImagenesBotones/chupete.png")
+nacimientoPhoto = tk.PhotoImage(file="Images/ImagenesBotones/Nacimiento.png")
 nacimientoImage = nacimientoPhoto.subsample(1, 1)
 
-cedulaPhoto = tk.PhotoImage(file="Images/ImagenesBotones/cedula.png")
+cedulaPhoto = tk.PhotoImage(file="Images/ImagenesBotones/Cedula.png")
 cedulaImage = cedulaPhoto.subsample(1, 1)
 
-matrimonioPhoto = tk.PhotoImage(file="Images/ImagenesBotones/Anillo SVG.png")
-matrimonioImage = matrimonioPhoto.subsample(3, 3)
+matrimonioPhoto = tk.PhotoImage(file="Images/ImagenesBotones/Matrimonio.png")
+matrimonioImage = matrimonioPhoto.subsample(1, 1)
 
-divorcioPhoto = tk.PhotoImage(file="Images/ImagenesBotones/a.png")
-divorcioImage = divorcioPhoto.subsample(3, 3)
+divorcioPhoto = tk.PhotoImage(file="Images/ImagenesBotones/Divorcio.png")
+divorcioImage = divorcioPhoto.subsample(1, 1)
 
-defuncionPhoto = tk.PhotoImage(file="Images/ImagenesBotones/MuelteChikita.png")
+defuncionPhoto = tk.PhotoImage(file="Images/ImagenesBotones/Difunto.png")
 defuncionImage = defuncionPhoto.subsample(1, 1)
 
 # Botones
 
-buttonActaNacimiento = tk.Button (root, text="\nActa\nde\nNacimiento",image = nacimientoImage,compound = tk.TOP, background= "WHITE")
-buttonActaNacimiento.config(height = 125, width = 130, font=("DM Sans", 10, "bold"))
+buttonActaNacimiento = tk.Button (frameMenu, text = "\nActa\nde\nNacimiento", image = nacimientoImage, compound = tk.TOP, background= "#209cb4", relief= tk.FLAT)
+buttonActaNacimiento.config(height = 125, width = 130, font=("Rubik", 10, "normal",),fg="WHITE",activebackground="#71acb7",activeforeground="WHITE")
 buttonActaNacimiento.grid(row=0,column =0)
 
-buttonCedula = tk.Button (root, text="\nCedula",image = cedulaImage,compound = tk.TOP,background= "WHITE")
-buttonCedula.config(height = 125, width = 130, font=("Comfortaa", 10, "bold"))
-buttonCedula.grid(row=1,column =0)
+buttonCedula = tk.Button (frameMenu, text = "\nCedula", image = cedulaImage, compound = tk.TOP, background= "#209cb4", relief= tk.FLAT)
+buttonCedula.config (height = 125, width = 130, font=("Rubik", 10, "normal"), fg="WHITE",activebackground="#71acb7",activeforeground="WHITE")
+buttonCedula.grid (row=1,column =0)
 
-buttonActaMatrimonio = tk.Button (root, text="\nActa\nde\nMatrimonio",image = matrimonioImage,compound = tk.TOP, background= "WHITE")
-buttonActaMatrimonio.config(height = 125, width = 130, font=("Garet Book", 10, "bold"))
+buttonActaMatrimonio = tk.Button (frameMenu, text = "\nActa\nde\nMatrimonio", image = matrimonioImage, compound = tk.TOP, background= "#209cb4", relief= tk.FLAT)
+buttonActaMatrimonio.config(height = 125, width = 130, font=("Rubik", 10, "normal"),fg="WHITE",activebackground="#71acb7",activeforeground="WHITE")
 buttonActaMatrimonio.grid(row=2,column =0)
 
-buttonActaDivorcio = tk.Button (root, text="\nActa \nde\nDivorcio", image = divorcioImage,compound = tk.TOP,background= "WHITE")
-buttonActaDivorcio.config(height = 125, width = 130, font=("nunito", 10, "bold"))
+buttonActaDivorcio = tk.Button (frameMenu, text = "\nActa \nde\nDivorcio", image = divorcioImage, compound = tk.TOP, background= "#209cb4", relief= tk.FLAT)
+buttonActaDivorcio.config(height = 125, width = 130, font=("Rubik", 10, "normal"),fg="WHITE", activebackground="#71acb7",activeforeground="WHITE")
 buttonActaDivorcio.grid(row=3,column =0)
 
-buttonActaDefuncion = tk.Button (root, text="\nActa \nde\nDefuncion", image = defuncionImage,compound = tk.TOP, relief= tk.FLAT, background= "WHITE")
-buttonActaDefuncion.configure(height = 125, width = 130, font=("Rubik", 10, "normal"))
+buttonActaDefuncion = tk.Button (frameMenu, text = "\nActa \nde\nDefuncion", image = defuncionImage, compound = tk.TOP, relief= tk.FLAT, background= "#209cb4")
+buttonActaDefuncion.configure(height = 126, width = 130, font=("Rubik", 10, "normal"),fg="WHITE", activebackground="#71acb7", activeforeground="WHITE")
 buttonActaDefuncion.grid(row = 4,column = 0)
 
 root.mainloop()
+
